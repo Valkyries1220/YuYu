e68b289da6aa192dc8e0cac6bdfdff7c5fe0a27a
valkyries <18067815020@163.com>
2025-03-03 09:41:28 +0800

    init 后端

---
+++ YuYu/src/main/java/com/qqyy/YuYu/YuYuApplication.java
@@ -0,0 +1,13 @@
+package com.qqyy.YuYu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YuYuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YuYuApplication.class, args);
+	}
+
+}
